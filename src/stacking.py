ac6ec2b8f1a72489ae8d0fd987d2ffaa228d83c1
lxy <815956033@qq.com>
2023-04-29 23:15:46 +0800

    2023-04-29

---
+++ src/stacking.py
@@ -0,0 +1,95 @@
+"""
+
+配置环境：conda install scikit-learn
+
+"""
+from sklearn.datasets import make_classification
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import train_test_split
+
+from sklearn.ensemble import GradientBoostingClassifier as GBDT
+from sklearn.ensemble import ExtraTreesClassifier as ET
+from sklearn.ensemble import RandomForestClassifier as RF
+from sklearn.ensemble import AdaBoostClassifier as ADA
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
+import numpy as np
+
+# 自动生成6000个样本,每个样本有2个类别
+x,y = make_classification(n_samples=6000)
+x_train,x_test,y_train,y_test = train_test_split(x,y, test_size=0.5)
+
+# 第一层模型
+clfs = [GBDT(n_estimators=100),# Classifiers
+        RF(n_estimators=100),
+        ET(n_estimators=100),
+        ADA(n_estimators=100)]
+
+#GBDT分类器
+clf_1 = clfs[0]
+clf_1.fit(x_train,y_train)
+# 取所有预测3000行的第二列,因为y_test就是第二列的结果
+pred_1 = clf_1.predict_proba(x_test)[ :,1]
+print("GBDT",roc_auc_score(y_test,pred_1))
+
+
+#随机森林分类器
+clf_2 = clfs[1]
+clf_2.fit(x_train,y_train)
+pred_2 = clf_2.predict_proba(x_test)[:,1]
+print("随机森林",roc_auc_score(y_test,pred_2))
+
+#ExtraTrees分类器
+clf_3 = clfs[2]
+clf_3.fit(x_train,y_train)
+pred_3 = clf_3.predict_proba(x_test)[:,1]
+print("ExtraTrees",roc_auc_score(y_test,pred_3))
+
+#AdaBoost分类器
+clf_4 = clfs[3]
+clf_4.fit(x_train,y_train)
+pred_4 = clf_4.predict_proba(x_test)[:,1]
+print("AdaBoost",roc_auc_score(y_test,pred_4))
+
+# x_train_stack:(3000,4),4个模型
+x_train_stack = np.zeros((x_train.shape[0],len(clfs)))
+# x_test_stack:(3000,4)
+x_test_stack = np.zeros((x_test.shape[0],len(clfs)))
+
+
+#6折stacking
+n_folds = 6
+# 我感觉skf是一个分类算法,把数据分成6份,每1份数据包含一组(x_train, y_train)
+skf = StratifiedKFold(n_splits=n_folds,shuffle=True,random_state=1)
+for i,clf in enumerate(clfs):
+    # x_stack_test_n:(3000,6)
+    x_stack_test_n = np.zeros((x_test.shape[0],n_folds))
+    for j, (train_index, test_index) in enumerate(skf.split(x_train, y_train)):
+        # train_index:(2500),train:5*500,test:1*500
+        tr_x = x_train[train_index] # 获得2500个训练样本
+        tr_y = y_train[train_index] # 获得2500个标签
+
+        clf.fit(tr_x,tr_y)
+        # 获得本次500个测试样本的预测数据,一共6折,所以刚好3000个预测结果
+        # 将1个模型的6个子模型concat到1个向量中,4个模型是4个向量,结果拼成一个(3000,4)的一个矩阵
+        x_train_stack[test_index,i] = clf.predict_proba(x_train[test_index])[:, 1]
+        # 对当前折的模型进行3000个测试样本预测,结果的第二列存起来,1个模型要存6次
+        x_stack_test_n[:, j] = clf.predict_proba(x_test)[:, 1]
+
+    # 对1个模型的6次3000样本测试结果,进行一个取均值,结果存起来
+    # 一共4个模型,x_test_stack:(3000,4)
+    x_test_stack[:, i] = x_stack_test_n.mean(axis=1)
+
+
+#第二层模型LR,主要负责找出4个模型的优缺点,可以不准确的理解为,就是找4个模型计算结果各自的权重
+clf_second = LogisticRegression(solver="lbfgs")
+# x_train_stack:(3000,4),y_train(3000)
+clf_second.fit(x_train_stack,y_train)
+pred = clf_second.predict_proba(x_test_stack)[:,1]
+print("Stacking",roc_auc_score(y_test,pred))
+
+
+
+
+
--- src/utils/fmix.py
+++ src/utils/fmix.py
@@ -9,7 +9,7 @@ from scipy.stats import beta
 
 
 def fftfreqnd(h, w=None, z=None):
-    """ Get bin values for discrete fourier transform of size (h, w, z)
+    """求出频域 F的一个参数:频率
 
     :param h: Required, first dimension size
     :param w: Optional, second dimension size
@@ -37,35 +37,50 @@ def fftfreqnd(h, w=None, z=None):
 
 
 def get_spectrum(freqs, decay_power, ch, h, w=0, z=0):
-    """ Samples a fourier image with given size and frequencies decayed by decay power
+    """ 生成过滤掉高频,随机高斯分布的z
 
-    :param freqs: Bin values for the discrete fourier transform
-    :param decay_power: Decay power for frequency decay prop 1/f**d
-    :param ch: Number of channels for the resulting mask
-    :param h: Required, first dimension size
-    :param w: Optional, second dimension size
-    :param z: Optional, third dimension size
+    Args:
+        :param freqs: Bin values for the discrete fourier transform,过滤高频所需的一个超参数
+        :param decay_power: Decay power for frequency decay prop 1/f**d,过滤高频所需的零一个超参数,控制高频阙值
+        :param ch: Number of channels for the resulting mask,z的通道
+        :param h: Required, first dimension size,z的高
+        :param w: Optional, second dimension size,z的宽
+        :param z: Optional, third dimension size,z也能是立体的
+
+    Returns:
+        (ch,h,w,z=0):一个基于高斯分布采样出的矩阵z,并且过滤掉了高频
+
     """
+    # 通过×一个分数来对z所有数进行缩小,从而过滤掉高频,而scale就是这个缩小分数
     scale = np.ones(1) / (np.maximum(freqs, np.array([1. / max(w, h, z)])) ** decay_power)
 
     param_size = [ch] + list(freqs.shape) + [2]
+    # 从高斯分布上随机采样出z
     param = np.random.randn(*param_size)
 
     scale = np.expand_dims(scale, -1)[None, :]
-
+    #相乘过滤掉高频
     return scale * param
 
 
 def make_low_freq_image(decay, shape, ch=1):
-    """ Sample a low frequency image from fourier space
+    """ 得到fmix的裁剪矩阵mask
 
-    :param decay_power: Decay power for frequency decay prop 1/f**d
-    :param shape: Shape of desired mask, list up to 3 dims
-    :param ch: Number of channels for desired mask
+    Args:
+        :param decay_power: Decay power for frequency decay prop 1/f**d
+        :param shape: Shape of desired mask, list up to 3 dims
+        :param ch: Number of channels for desired mask
+
+    Returns:
+        (1,w,h): 返回二值化前的mask矩阵
     """
+
     freqs = fftfreqnd(*shape)
+    # 获得随机采样出、过滤掉高频的z
     spectrum = get_spectrum(freqs, decay, ch, *shape)#.reshape((1, *shape[:-1], -1))
+    # 将z的实部维度与虚部维度相加,得到真正的z
     spectrum = spectrum[:, 0] + 1j * spectrum[:, 1]
+    # 将z进行傅里叶逆变换,取出实数部分,得到最后的mask,shape=(1,w,h)
     mask = np.real(np.fft.irfftn(spectrum, shape))
 
     if len(shape) == 1:
@@ -75,6 +90,7 @@ def make_low_freq_image(decay, shape, ch=1):
     if len(shape) == 3:
         mask = mask[:1, :shape[0], :shape[1], :shape[2]]
 
+    # 对mask进行归一化,归一化到(0,1)之间
     mask = mask
     mask = (mask - mask.min())
     mask = mask / mask.max()
@@ -82,10 +98,12 @@ def make_low_freq_image(decay, shape, ch=1):
 
 
 def sample_lam(alpha, reformulate=False):
-    """ Sample a lambda from symmetric beta distribution with given alpha
+    """ 从β分布中抽样出lam
 
-    :param alpha: Alpha value for beta distribution
-    :param reformulate: If True, uses the reformulation of [1].
+    Args:
+        :param alpha: Alpha value for beta distribution
+        :param reformulate: If True, uses the reformulation of [1].
+
     """
     if reformulate:
         lam = beta.rvs(alpha+1, alpha) # rvs(arg1,arg2,loc=期望, scale=标准差, size=生成随机数的个数) 从分布中生成指定个数的随机数
@@ -96,13 +114,14 @@ def sample_lam(alpha, reformulate=False):
 
 
 def binarise_mask(mask, lam, in_shape, max_soft=0.0):
-    """ Binarises a given low frequency image such that it has mean lambda.
+    """ 对mask进行二值化处理
 
-    :param mask: Low frequency image, usually the result of `make_low_freq_image`
-    :param lam: Mean value of final mask
-    :param in_shape: Shape of inputs
-    :param max_soft: Softening value between 0 and 0.5 which smooths hard edges in the mask.
-    :return:
+    Args:
+        :param mask: Low frequency image, usually the result of `make_low_freq_image`
+        lam (int): Mean value of final mask,我感觉是二值化的阙值
+        :param in_shape: Shape of inputs
+        param max_soft (float): 让0-1边界变得很柔和,Softening value between 0 and 0.5 which smooths hard edges in the mask.
+        :return:
     """
     idx = mask.reshape(-1).argsort()[::-1]
     mask = mask.reshape(-1)
@@ -125,7 +144,7 @@ def binarise_mask(mask, lam, in_shape, max_soft=0.0):
 
 
 def sample_mask(alpha, decay_power, shape, max_soft=0.0, reformulate=False):
-    """ Samples a mean lambda from beta distribution parametrised by alpha, creates a low frequency image and binarises
+    """ 得到二值化后的mask,Samples a mean lambda from beta distribution parametrised by alpha, creates a low frequency image and binarises
     it based on this lambda
 
     :param alpha: Alpha value for beta distribution from which to sample mean of mask
@@ -137,18 +156,19 @@ def sample_mask(alpha, decay_power, shape, max_soft=0.0, reformulate=False):
     if isinstance(shape, int):
         shape = (shape,)
 
-    # Choose lambda
+    # Choose lambda,二值化的阙值
     lam = sample_lam(alpha, reformulate)
 
     # Make mask, get mean / std
     mask = make_low_freq_image(decay_power, shape)
+    # 二值化mask,是否羽化由max_soft决定
     mask = binarise_mask(mask, lam, shape, max_soft)
 
     return lam, mask
 
 
 def sample_and_apply(x, alpha, decay_power, shape, max_soft=0.0, reformulate=False):
-    """
+    """ 随机抽取同一batch的图片,和原图进行fmix
 
     :param x: Image batch on which to apply fmix of shape [b, c, shape*]
     :param alpha: Alpha value for beta distribution from which to sample mean of mask
